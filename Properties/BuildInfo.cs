1581082b0892b8d86c75bb1aad952e5fee5d86bc
MrSago <37180898+MrSago@users.noreply.github.com>
2025-01-02 00:00:04 +0300

    fix: optimize and fixes

--- Properties/BuildInfo.cs
+++ Properties/BuildInfo.cs
@@ -6,9 +6,9 @@ internal static class BuildInfo
     public const string DESCRIPTION = "Unlocks the ability to sit down";
     public const string AUTHOR = "MrS4g0";
     public const string COMPANY = null;
-    public const string VERSION = "1.0.0";
+    public const string VERSION = "1.0.1";
     public const string GAMEVERSION = null;
     public const string DOWNLOADLINK = "https://github.com/MrSago/MiSide-Sit-Unlocker";
-    public const string PACKAGE = "com.situnlocker.miside";
+    public const string PACKAGE = "com.miside.situnlocker";
     public const string GUID = "e2cf0c0d-5d3e-489f-8fa2-45a5814a46c2";
 }
--- SitUnlocker.cs
+++ SitUnlocker.cs
@@ -30,6 +30,8 @@ public static class SitUnlocker
         }
     }
 
+    private static PlayerMove? _playerMove;
+
     public static void Init()
     {
         if (Enabled)
@@ -44,11 +46,13 @@ public static class SitUnlocker
     {
         try
         {
-            PlayerMove? playerMove = UnityEngine.Object.FindObjectOfType<PlayerMove>();
-            if (playerMove is not null)
+            PlayerMove? playerMove = GetPlayerMove();
+            if (playerMove == null)
             {
-                playerMove.canSit = value;
+                return;
             }
+
+            playerMove.canSit = value;
         }
         catch (Exception e)
         {
@@ -62,5 +66,19 @@ public static class SitUnlocker
         {
             SetPlayerSitState(true);
         }
+    }
+
+    private static PlayerMove? GetPlayerMove()
+    {
+        if (!IsPlayerMoveValid())
+        {
+            _playerMove = GameObject.Find("Player")?.GetComponent<PlayerMove>();
+        }
+        return _playerMove;
+    }
+
+    private static bool IsPlayerMoveValid()
+    {
+        return _playerMove != null && _playerMove.gameObject != null;
     }
 }
